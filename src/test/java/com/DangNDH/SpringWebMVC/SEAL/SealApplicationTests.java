64555c47e45b21b9a9294014d3f7cc06a8b80ba9
Kiru Nguyen <nguyendinhhaidang15@gmail.com>
2026-06-13 13:57:53 +0700

    chore: initial setup - use Spring Initializr (Maven/Java 21/Spring 4.1.0/JAR) + Lombok + Spring Data JPA + PostgreSQL Driver + Spring Configuration Processor + Spring Web + Thymeleaf + Validation - add IDEA configuration

---
+++ src/test/java/com/DangNDH/SpringWebMVC/SEAL/SealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DangNDH.SpringWebMVC.SEAL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
